3774d24c412508cc0fab0763d118666b543c8120
sfan5 <sfan5@live.de>
2021-10-11 18:40:59 +0200

    buildscripts: add python download (for the time being)

--- buildscripts/include/download-deps.sh
+++ buildscripts/include/download-deps.sh
@@ -66,3 +66,9 @@ HEREDOC
 [ ! -d mpv ] && git clone https://github.com/mpv-player/mpv
 
 cd ..
+
+# youtube-dl
+$WGET https://kitsunemimi.pw/ytdl/dist.zip
+unzip dist.zip -d ../app/src/main/assets/ytdl
+rm -f ../app/src/main/assets/ytdl/youtube-dl # don't need it
+rm dist.zip
